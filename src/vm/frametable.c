836bd91d2c6280d3b6ab8de3c5bf571d3ddc9872
radu prunoiu <radu@cs.utexas.edu>
2015-03-31 17:54:16 -0500

    Completed frame table commit

---
+++ src/vm/frametable.c
@@ -0,0 +1,56 @@
+#include "frametable.h"
+#define NUMFRAMES 1111
+#include "threads/palloc.h"
+
+//extern uint32_t init_ram_pages;
+
+struct metaframe frametable[NUMFRAMES]; 
+
+struct metaframe* 
+get_metaframe_byindex(int index){
+	return &frametable[index];
+}
+
+struct metaframe* 
+get_metaframe_bypage(void* page){
+	int i;
+	for(i = 0; i < NUMFRAMES; i++){
+		if(frametable[i].page == page){
+			return &frametable[i];
+		}
+	}
+	return NULL;
+}
+
+struct metaframe* 
+next_empty_frame(){
+	int i;
+	for(i=0; i<NUMFRAMES; i++){
+		if(!frametable[i].isfilled)
+			return &frametable[i];
+	}
+	return NULL;
+}
+
+void* 
+assign_page(){
+	void* new_page = palloc_get_page(PAL_USER | PAL_ZERO);
+	if(new_page==NULL)
+		PANIC("no available pages");
+	struct metaframe* new_frame = next_empty_frame();
+	if(new_frame==NULL)
+		PANIC("no empty frames available");
+	new_frame->page = new_page;
+	new_frame->isfilled = true;
+	return new_page;
+}
+
+void
+free_frame(void* page){
+	struct metaframe* frame2free = get_metaframe_bypage(page);
+	ASSERT(frame2free != NULL);
+
+	frame2free->isfilled = false;
+	palloc_free_page(frame2free->page);
+}
+
---
+++ src/vm/frametable.h
@@ -0,0 +1,28 @@
+#ifndef VM_FRAMETABLE_H
+#define VM_FRAMETABLE_H
+
+#include <stdbool.h>
+#include <debug.h>
+
+
+
+//meta data for a frame in the frame table 
+struct metaframe
+{
+	bool isfilled;
+	void *page;	
+};
+
+//get a metaframe in the table by page
+struct metaframe* get_metaframe_bypage(void* page);
+//get the metaframe at an index within the frame table
+struct metaframe* get_metaframe_byindex(int index);
+//get the next available metaframe within the frame table
+struct metaframe* next_empty_frame(void);
+//assign a page for the frame
+void* assign_page(void);
+//free up a frame
+void free_frame(void* page);
+
+
+#endif /* vm/frametable.h */
